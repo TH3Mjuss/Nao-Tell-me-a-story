0de88aedd3ea3748fe95517144e0f159395aff06
Mehdi <mehdi.jusseaume@gmail.com>
2016-02-22 11:50:19 +0100

    Tell me a story
    Initial commit of the app “Tell me a story”.  This contains the first
    version of the french story “Le vieux grand-père”

---
+++ pkg/Le vieux grand-père.pml
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="Le vieux grand-père" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="Le vieux grand-père" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="icon" src="icon.png" />
+        <File name="" src=".DS_Store" />
+        <File name="Le vieux grand-père" src="Le vieux grand-père.pml" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths>
+        <Path src="levieuxgrand-pre-0f865e-0.1.0.pkg" />
+    </IgnoredPaths>
+</Package>
---
+++ src/Le vieux grand-père.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from naoqi import ALProxy
+
+"""
+\pau=xxx\    --> pause time (in ms)
+\vct=xxx\    --> manage voice pitch 50 to 200% (default 100%)
+\rspd=xxx\   --> voice speed 50 to 400% (default 100%)
+\vol=xxx\    --> manage volume 1to 100%
+"""
+
+IP = "XXX.XXX.XX.XX" #Your IP here
+tts = ALProxy("ALTextToSpeech", IP, 9559)
+tts.setLanguage("French")
+tts.say("\\Vol=100\\homme !")
+tts.say("Il était une fois un pauvre homme bien vieux, \\pau=200\\qui avait les yeux trouble,\\pau=200\\ "\
+        "l’oreille dure et les genoux tremblants. \\pau=600\\Quand il était à table,\\pau=200\\ il pouvait à peine tenir "\
+        "sa cui yère ;\\pau=450\\ il reaipandait de la soupe sur la nappe,\\pau=200\\ et quelquefois mème en laissait échapper "\
+        "de sa bouche.\\pau=600\\ La femme de son fils et son fils lui-mème en avaient pris un grand \\rspd=85\\daigoût, \\pau=200\\"\
+        "\\rspd=100\\et à la fin ils le reléguaire dans un coin derrière le poil, \\pau=200\\ où ils lui donnaient à manger une "\
+        "chaitive pitance dans une vieille écuelle de terre.\\pau=600\\ Le vieillard avaient souvent les larmes aux yeux,\\pau=200\\ "\
+        "et regardait tristement du coter de la table...\\pau=1000\\")
+tts.say("\Un jour, \\pau=200\\ l'écuelle,\\pau=200\\ que tenaient mal ses mains tremblantes,\\pau=200\\ tomba À terre et se brisa."\
+        "\\pau=600\\La jeune femme s'emporta en reproches:\\pau=400\\ il n'osa rien répondre et baissa la tête en soupirant.\\pau=600\\"\
+        "On lui \\rspd=90\\acheuta \\rspd=100\\ pour deux liards une écuelle de bois dans laquelle désormais on lui donnait à manger."\
+        "\\pau=1000\\")
+tts.say("Quelques jours après,\\pau=200\\ son fils et sa belle-fille virent leur enfant qui avait quatre ans,\\pau=200\\ occupé \\pau=120\\"\
+        "à assembler par terre de petites planchettes.\\pau=600\\ \\rspd=95\\ \\vct=70\\Que fais-tu là ?\\vct=100\\ \\rspd=100\\ Demanda "\
+        "son père.\\pau=600\\ \\vct=150\\C'est un auget, \\pau=200\\ \\vct=100\\ répondit-il,\\pau=200\\ \\vct=150\\pour donner À manger "\
+        "à papa et maman quand ils seront vieux.\\vct=100\\ \\pau=1000\\")
+tts.say("Le mari et la femme se regardère un instant sans rien dire,\\pau=200\\ puis ils se mirent à pleurer, \\pau=200\\ reprirent le "\
+        "vieux grand-père à table,\\pau=200\\ et désormais le firent toujours manger à table avec eux,\\pau=200\\ sans plus jamais le rudoyer.")
+
+print("\n Done.\n")
